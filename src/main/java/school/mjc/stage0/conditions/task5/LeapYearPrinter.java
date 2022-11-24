5dcc94fdd89a3637dbd68c3028a1ad770fd1daaa
artem_shinkevich <SKILLBOX2021oNe>
2022-11-24 22:23:09 +0300

    All tasks done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,28 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+//        System.out.println((year % 100 != 0 && year % 4 == 0 || year % 400 == 0) ? "leap" : "not leap");
+        switch (year % 100) {
+            case 0:
+                switch (year % 400) {
+                    case 0:
+                        System.out.println("leap");
+                        break;
+                    default:
+                        System.out.println("not leap");
+                        break;
+                }
+                break;
+            default:
+                switch (year % 4) {
+                    case 0:
+                        System.out.println("leap");
+                        break;
+                    default:
+                        System.out.println("not leap");
+                        break;
+                }
+                break;
+        }
     }
 }
